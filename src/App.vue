2a77de912b07f08c1899b5c5b58e7f85c17fc84a
talzakai <talzakai7@gmail.com>
2022-09-12 15:50:20 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+//import { RouterLink, RouterView } from 'vue-router'
+import { defineComponent } from "vue";
+import Products from "../src/components/products.vue";
+import AppHeader from "./components/appHeader.vue";
+export default defineComponent({
+  components: { Products, AppHeader },
+});
+</script>
+
+<template>
+  <AppHeader />
+  <Products />
+  <!--<RouterView />-->
+</template>
+
+<style scoped>
+    
+</style>
+
---
+++ src/components/products.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import { injectProp } from "@vue/compiler-core";
+import { defineComponent } from "vue";
+import Product from "./product.vue";
+export default defineComponent({
+  components: { Product },
+});
+</script>
+<template>
+  <section class="products">
+    <Product />
+    <Product />
+    <Product />
+    <Product />
+    <Product />
+    <Product />
+    <Product />
+    <Product />
+    <Product />
+    <Product />
+  </section>
+</template>
+<style scoped>
+.products {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+@media (max-width: 920px) {
+  .products .product-card:first-child,
+  .products .product-card:nth-child(2) {
+    flex: 2 46%;
+  }
+}
+</style>
